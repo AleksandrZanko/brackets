4f63067dbc4b2afe693bad31055896c3d5ab25cd
Aleksandr Zanko <58461608+AleksandrZanko@users.noreply.github.com>
2020-10-13 12:01:23 +0300

    add some test resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let leftSc = 0;
+  let rightSc = 0;
+  let leftKv = 0;
+  let rightKv = 0;
+  let vertical = 0;
+  for(let i = 0; i < str.length; i++) {
+  	if(str[i] == '(') {
+  		leftSc++;
+  	} else if(str[i] == ')') {
+  		rightSc++;
+  	} else if(str[i] == '[') {
+  		leftKv++;
+  	} else if (str[i] == ']') {
+  		rightKv++;
+  	} else if (str[i] == '|') {
+  		vertical++;
+  	}
+  }
+
+  let verticalResult = vertical % 2;
+
+  if((leftSc == rightSc) && (leftKv == rightKv) && verticalResult == 0) {
+  	return true;
+  } else {
+  	return false;
+  }
 }
